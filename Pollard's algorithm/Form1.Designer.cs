a569711c1f19f47248fe33356782cbde97c1ffaa
TBO9l-47-XPOMOCOMA <72855442+TBO9l-47-XPOMOCOMA@users.noreply.github.com>
2021-04-20 18:19:05 +0300

    Add files via upload

---
+++ Pollard's algorithm/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace Pollard_s_algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.e_textBox = new System.Windows.Forms.TextBox();
+            this.p_textBox = new System.Windows.Forms.TextBox();
+            this.crypto_textBox = new System.Windows.Forms.TextBox();
+            this.d_textBox = new System.Windows.Forms.TextBox();
+            this.q_textBox = new System.Windows.Forms.TextBox();
+            this.n_textBox = new System.Windows.Forms.TextBox();
+            this.hack_button = new System.Windows.Forms.Button();
+            this.result_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.time_label = new System.Windows.Forms.Label();
+            this.iterations_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // e_textBox
+            // 
+            this.e_textBox.Location = new System.Drawing.Point(56, 25);
+            this.e_textBox.Name = "e_textBox";
+            this.e_textBox.Size = new System.Drawing.Size(551, 22);
+            this.e_textBox.TabIndex = 0;
+            // 
+            // p_textBox
+            // 
+            this.p_textBox.Location = new System.Drawing.Point(56, 80);
+            this.p_textBox.Name = "p_textBox";
+            this.p_textBox.ReadOnly = true;
+            this.p_textBox.Size = new System.Drawing.Size(551, 22);
+            this.p_textBox.TabIndex = 1;
+            // 
+            // crypto_textBox
+            // 
+            this.crypto_textBox.Location = new System.Drawing.Point(56, 220);
+            this.crypto_textBox.Multiline = true;
+            this.crypto_textBox.Name = "crypto_textBox";
+            this.crypto_textBox.Size = new System.Drawing.Size(1216, 273);
+            this.crypto_textBox.TabIndex = 2;
+            // 
+            // d_textBox
+            // 
+            this.d_textBox.Location = new System.Drawing.Point(394, 160);
+            this.d_textBox.Name = "d_textBox";
+            this.d_textBox.ReadOnly = true;
+            this.d_textBox.Size = new System.Drawing.Size(551, 22);
+            this.d_textBox.TabIndex = 6;
+            // 
+            // q_textBox
+            // 
+            this.q_textBox.Location = new System.Drawing.Point(721, 80);
+            this.q_textBox.Name = "q_textBox";
+            this.q_textBox.ReadOnly = true;
+            this.q_textBox.Size = new System.Drawing.Size(551, 22);
+            this.q_textBox.TabIndex = 5;
+            // 
+            // n_textBox
+            // 
+            this.n_textBox.Location = new System.Drawing.Point(721, 25);
+            this.n_textBox.Name = "n_textBox";
+            this.n_textBox.Size = new System.Drawing.Size(551, 22);
+            this.n_textBox.TabIndex = 4;
+            // 
+            // hack_button
+            // 
+            this.hack_button.Location = new System.Drawing.Point(636, 508);
+            this.hack_button.Name = "hack_button";
+            this.hack_button.Size = new System.Drawing.Size(68, 34);
+            this.hack_button.TabIndex = 8;
+            this.hack_button.Text = "hack";
+            this.hack_button.UseVisualStyleBackColor = true;
+            this.hack_button.Click += new System.EventHandler(this.hack_button_Click);
+            // 
+            // result_textBox
+            // 
+            this.result_textBox.Location = new System.Drawing.Point(56, 565);
+            this.result_textBox.Multiline = true;
+            this.result_textBox.Name = "result_textBox";
+            this.result_textBox.ReadOnly = true;
+            this.result_textBox.Size = new System.Drawing.Size(1216, 181);
+            this.result_textBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "E";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "P";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1300, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "N";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(655, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "D";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(1299, 80);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(19, 17);
+            this.label.TabIndex = 14;
+            this.label.Text = "Q";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(645, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Crypto";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(646, 545);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Result";
+            // 
+            // time_label
+            // 
+            this.time_label.AutoSize = true;
+            this.time_label.Location = new System.Drawing.Point(1089, 749);
+            this.time_label.Name = "time_label";
+            this.time_label.Size = new System.Drawing.Size(43, 17);
+            this.time_label.TabIndex = 18;
+            this.time_label.Text = "Time:";
+            // 
+            // iterations_label
+            // 
+            this.iterations_label.AutoSize = true;
+            this.iterations_label.Location = new System.Drawing.Point(53, 751);
+            this.iterations_label.Name = "iterations_label";
+            this.iterations_label.Size = new System.Drawing.Size(70, 17);
+            this.iterations_label.TabIndex = 19;
+            this.iterations_label.Text = "Iterations:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1330, 777);
+            this.Controls.Add(this.iterations_label);
+            this.Controls.Add(this.time_label);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result_textBox);
+            this.Controls.Add(this.hack_button);
+            this.Controls.Add(this.d_textBox);
+            this.Controls.Add(this.q_textBox);
+            this.Controls.Add(this.n_textBox);
+            this.Controls.Add(this.crypto_textBox);
+            this.Controls.Add(this.p_textBox);
+            this.Controls.Add(this.e_textBox);
+            this.Name = "Form1";
+            this.Text = "Pollard\'s algorithm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox e_textBox;
+        private System.Windows.Forms.TextBox p_textBox;
+        private System.Windows.Forms.TextBox crypto_textBox;
+        private System.Windows.Forms.TextBox d_textBox;
+        private System.Windows.Forms.TextBox q_textBox;
+        private System.Windows.Forms.TextBox n_textBox;
+        private System.Windows.Forms.Button hack_button;
+        private System.Windows.Forms.TextBox result_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label time_label;
+        private System.Windows.Forms.Label iterations_label;
+    }
+}
+
---
+++ Pollard's algorithm/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace Pollard_s_algorithm
+{
+    public partial class Form1 : Form
+    {
+        public string alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя";
+        Stopwatch time = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public List<BigInteger> euclid(BigInteger a, BigInteger b)
+        {
+            List<BigInteger> bxy = new List<BigInteger> { };
+            BigInteger c = 1;
+            List<BigInteger> div_list = new List<BigInteger> { };
+            List<BigInteger> x_list = new List<BigInteger> { 0 };
+            List<BigInteger> y_list = new List<BigInteger> { 1 };
+            if (a % b != 0)
+            {
+                while (true)
+                {
+                    div_list.Add(a / b);
+                    c = a % b;
+                    a = b;
+                    b = c;
+                    if (a % b == 0)
+                    {
+                        div_list.Add(a / b);
+                        break;
+                    }
+
+                }
+                bxy.Add(b);
+                for (int i = 1; i < div_list.Count; i++)
+                {
+                    x_list.Add(y_list[i - 1]);
+                    y_list.Add(x_list[i - 1] - y_list[i - 1] * div_list[div_list.Count - i - 1]);
+                }
+            }
+            else
+            {
+                bxy.Add(b);
+            }
+            bxy.Add(x_list[x_list.Count - 1]);
+            bxy.Add(y_list[y_list.Count - 1]);
+            return bxy;
+        }
+        public BigInteger exp(BigInteger a, BigInteger e, BigInteger n)
+        {
+            BigInteger cloneE = e;
+            BigInteger divisor = 2;
+            List<BigInteger> c_exp = new List<BigInteger> { a };
+            List<BigInteger> degreeToBinary = new List<BigInteger> { };
+            while (true)
+            {
+                BigInteger trash;
+                BigInteger.DivRem(cloneE, divisor, out trash);
+                degreeToBinary.Add(trash);
+                if (cloneE == 1)
+                    break;
+                cloneE /= 2;
+            }
+            degreeToBinary.Reverse();
+            for (int i = 1; i < degreeToBinary.Count; i++)
+            {
+                c_exp.Add((c_exp[i - 1] * c_exp[i - 1] * BigInteger.Pow(a, int.Parse(degreeToBinary[i].ToString()))) % n);
+            }
+            BigInteger result = c_exp[c_exp.Count - 1];
+            return result;
+        }
+        public bool millerRabin(BigInteger r_mr)
+        {
+
+            if (r_mr == 2 || r_mr == 3)
+                return true;
+            if (r_mr < 2 || r_mr % 2 == 0)
+                return false;
+            BigInteger k_mr = (BigInteger)BigInteger.Log(r_mr, 2.0);
+            BigInteger t = r_mr - 1;
+            BigInteger s = 0;
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s += 1;
+            }
+            for (BigInteger i = 0; i < k_mr; i++)
+            {
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                byte[] forA = new byte[r_mr.ToByteArray().LongLength];
+                BigInteger a;
+                do
+                {
+                    rng.GetBytes(forA);
+                    a = new BigInteger(forA);
+                }
+                while (a < 2 || a > r_mr - 2);
+
+                if (r_mr % a == 0)
+                    return false;
+
+                BigInteger b = exp(a, t, r_mr);
+                if (b == 1)
+                    continue;
+                for (BigInteger j = 1; j < s; j++)
+                {
+                    b = exp(b, 2, r_mr);
+                    if (b == r_mr - 1)
+                        break;
+                }
+                if (b != r_mr - 1)
+                    return false;
+
+            }
+            return true;
+        }
+        private BigInteger pollard(BigInteger n)
+        {
+            
+            BigInteger i = 1, xi = 2, y = 1,gcd = 1, next_y = 1, diff; 
+            int k = 0;
+            while (gcd <= 1)
+            {
+                if (i == 1)
+                {                  
+                    diff = BigInteger.Abs(xi - y);
+                    gcd = BigInteger.GreatestCommonDivisor(n, diff);
+                    i++;
+                    next_y = xi;
+                    k++;
+                }
+                else
+                {
+                    xi = (BigInteger.Pow(xi, 2) - 1) % n;
+                    y = next_y;
+                    diff = BigInteger.Abs(xi - y);
+                    try
+                    {
+                        gcd = BigInteger.GreatestCommonDivisor(n, diff);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Попытка деления на 0, неверные значения для расшифровки!");
+                        return 1;
+                    }
+                    if (i == BigInteger.Pow(2, k))
+                    {
+                        next_y = xi;
+                        k++;
+                    }
+                    i++;
+                }
+            }
+            iterations_label.Text ="Iterations: " + (i - 1).ToString();
+            return gcd;
+        }
+
+        private void hack_button_Click(object sender, EventArgs e)
+        {
+            result_textBox.Text = string.Empty;
+            BigInteger N,E,crypto;
+            try
+            {
+                E = BigInteger.Parse(e_textBox.Text);
+                N = BigInteger.Parse(n_textBox.Text);
+                crypto = BigInteger.Parse(crypto_textBox.Text);
+                if (E <= 0 || N <= 999)
+                {
+                    MessageBox.Show("E > 0, N>999");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите корректные e,N и Crypto");
+                return;
+            }
+            string result = string.Empty;
+            time.Reset();
+            time.Start();
+            BigInteger p = pollard(N);
+            BigInteger q = N / p;
+            BigInteger Fn = (p - 1) * (q - 1);
+            BigInteger d = euclid(Fn, E)[2];
+            d = (d < 0) ? d + Fn : d;
+            BigInteger y = exp(crypto, d, N);
+            result = y.ToString() + ' ';
+            time.Stop();
+
+            time_label.Text = time.Elapsed.ToString();
+            p_textBox.Text = p.ToString();
+            q_textBox.Text = q.ToString();
+            d_textBox.Text = d.ToString();
+
+            if (y.ToString().Length % 2 == 0)
+            {
+                int iter = y.ToString().Length;
+                for (int i = (iter / 2) - 1; i >= 0; i--)
+                {
+                    BigInteger index = (BigInteger.Divide(y, BigInteger.Pow(100, i)) - 16) % alphabet.Length;
+                    if (index < 0) index += alphabet.Length;
+                    result += alphabet[Int32.Parse(index.ToString())];
+                    BigInteger new_y;
+                    BigInteger.DivRem(y, BigInteger.Pow(100, i), out new_y);
+                    y = new_y;
+                }
+            }
+            result_textBox.Text = result;
+
+
+        }
+    }
+}
